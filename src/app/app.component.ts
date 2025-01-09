2c75cbd9d6887c4b398821ed17c870684acbb231
Samo1999382 <hamadaarafa1999382@gmail.com>
2025-01-09 18:08:25 +0200

    V0.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AboutComponent} from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProtofolioComponent } from "./components/protofolio/protofolio.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, HomeComponent, ContactComponent, NavbarComponent, FooterComponent, ProtofolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/protofolio/protofolio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protofolio',
+  imports: [],
+  templateUrl: './protofolio.component.html',
+  styleUrl: './protofolio.component.css'
+})
+export class ProtofolioComponent {
+
+}
